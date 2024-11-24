c0a569bd98240b8684a965fb1cedb4bffeff3b43
vitukjkkk <vittorcolom@gmail.com>
2024-11-24 15:21:13 -0300

    adicionando timers

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
 import '../css/index.css';
 
+import { srcMainElements } from './main';
+
 // ==============================
 // IMPORTANDO ARQUIVOS
 // ==============================
@@ -13,12 +15,56 @@ import '../assets/images/logo_sem_fundo.png';
 
 // MAIN
 
+// BANNERS
 import '../assets/banners/banner_ressurja.png';
+import '../assets/banners/banner_novidades.png';
+import '../assets/banners/banner_meio_ambiente.png';
+
 import '../assets/images/image_notices_sem_fundo.png';
+
+// SOCIAL MEDIA ICONS
 import '../assets/icons/social_media/zap_pequeno.png';
+import '../assets/icons/social_media/facebook_pequeno.png';
 import '../assets/icons/social_media/github_pequeno.png';
 import '../assets/icons/social_media/ig_pequeno.png';
 import '../assets/icons/social_media/linkedin_pequeno.png';
 import '../assets/icons/social_media/pinterest_pequeno.png';
 import '../assets/icons/social_media/tik-tok_pequeno.png';
 import '../assets/icons/social_media/youtube_pequeno.png';
+
+// ============================
+// ELEMENTOS
+// ============================
+
+const imgMainBanner = document.querySelector('#imgMainBanner') as HTMLImageElement;
+const h2MainBanners = document.querySelector('#h2MainBanners') as HTMLHeadingElement;
+const pMainBanners = document.querySelector('#pMainBanners') as HTMLParagraphElement;
+
+const buttonEnviarContato = document.querySelector('#buttonEnviarContato') as HTMLButtonElement;
+const inputMainContacts = document.querySelector('#inputMainContacts') as HTMLInputElement;
+
+// ============================
+// VARIABLES
+// ============================
+
+const MAX_BANNERS = 2;
+
+var indexMainBanner = 0;
+
+// ============================
+// TRIGGERS
+// ============================
+
+buttonEnviarContato.onclick = () => window.open('https://wa.me/5561994030749?text=' + inputMainContacts.value.replace(' ', '%20'), '_blank');
+
+window.onload = () => setInterval(() => changeMainBanner(indexMainBanner), 10000);
+
+// FUNCTIONS
+
+function changeMainBanner(index: number) {
+    imgMainBanner.src = srcMainElements.banners[index];
+    h2MainBanners.textContent = srcMainElements.titles[index];
+    pMainBanners.textContent = srcMainElements.messages[index];
+    if(indexMainBanner === MAX_BANNERS) return indexMainBanner = 0;
+    indexMainBanner++;
+}
---
+++ src/ts/main.ts
@@ -0,0 +1,23 @@
+const srcMainElements = {
+    banners : [
+        'images/banner_meio_ambiente.png',
+        'images/banner_novidades.png',
+        'images/banner_ressurja.png'
+    ],
+    
+    titles: [
+        'Meio Ambiente',
+        'Novidades',
+        'Ressurja'
+    ],
+
+    messages: [
+        'Aqui você encontra informações sobre o meio ambiente',
+        'Fique por dentro das novidades do mundo',
+        'Encare o desafio de ressurgir'
+    ]
+};
+
+export {
+    srcMainElements
+}
