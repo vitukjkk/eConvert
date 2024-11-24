c04d8ae7715a122ccce43c881aa3be80695ac6cd
vitukjkkk <vittorcolom@gmail.com>
2024-11-24 16:01:58 -0300

    terminando index

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -36,6 +36,11 @@ import '../assets/icons/social_media/youtube_pequeno.png';
 // ELEMENTOS
 // ============================
 
+// HEADER
+const divHeaderLeftSideR = document.querySelector('#divHeaderLeftSideR') as HTMLDivElement;
+const imgHeaderIcons = document.querySelectorAll<HTMLImageElement>('.imgHeaderIcons');
+
+// MAIN
 const imgMainBanner = document.querySelector('#imgMainBanner') as HTMLImageElement;
 const h2MainBanners = document.querySelector('#h2MainBanners') as HTMLHeadingElement;
 const pMainBanners = document.querySelector('#pMainBanners') as HTMLParagraphElement;
@@ -55,11 +60,38 @@ var indexMainBanner = 0;
 // TRIGGERS
 // ============================
 
+// CLICK
+
 buttonEnviarContato.onclick = () => window.open('https://wa.me/5561994030749?text=' + inputMainContacts.value.replace(' ', '%20'), '_blank');
+
+imgHeaderIcons.forEach((icon, index) => {
+    icon.onclick = () => {
+        switch(index) {
+            case 0:
+                if(divHeaderLeftSideR.style.display === 'none') {
+                    divHeaderLeftSideR.style.display = 'block';
+                } else {
+                    divHeaderLeftSideR.style.display = 'none';
+                }
+                break;
+            case 1:
+                window.location.href = './sobre.html';
+                break;
+            case 2:
+                window.location.href = './configuracoes.html';
+                break;
+        }
+    }
+});
+
+// LOAD
 
 window.onload = () => setInterval(() => changeMainBanner(indexMainBanner), 10000);
 
+
+// ===============================
 // FUNCTIONS
+// ===============================
 
 function changeMainBanner(index: number) {
     imgMainBanner.src = srcMainElements.banners[index];
