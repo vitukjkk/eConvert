65494f27b94468813e07ace7c3de601e1728ffa5
vitukjkkk <vittorcolom@gmail.com>
2024-12-02 15:41:11 -0300

    fazendo a lógica de conversão

--- src/ts/converter.ts
+++ src/ts/converter.ts
@@ -47,19 +47,26 @@ buttonConversions.forEach((button, index) => {
         switch(index) {
             case 0: // BOTÃO CONVERTER
                 switch(Number(selectMainConversions[0].value)) {
-                    case 1: //  MASSA
+                    case 1: // MASSA
+                        inputConvertResult.value = String(convertValue(1, selectMainConversions[1].value, selectMainConversions[8].value));
                         break;
                     case 2: // COMPRIMENTO
+                        inputConvertResult.value = String(convertValue(2, selectMainConversions[2].value, selectMainConversions[9].value));
                         break;
                     case 3: // TEMPERATURA
+                        inputConvertResult.value = String(convertValue(3, selectMainConversions[3].value, selectMainConversions[10].value));
                         break;
                     case 4: // VELOCIDADE
+                        inputConvertResult.value = String(convertValue(4, selectMainConversions[4].value, selectMainConversions[11].value));
                         break;
                     case 5: // ÁREA
+                        inputConvertResult.value = String(convertValue(5, selectMainConversions[5].value, selectMainConversions[12].value));
                         break;
                     case 6: // VOLUME
+                        inputConvertResult.value = String(convertValue(6, selectMainConversions[6].value, selectMainConversions[13].value));
                         break;
-                    case 7: // TEMPO
+                    case 7: // TEMPO    
+                        inputConvertResult.value = String(convertValue(7, selectMainConversions[7].value, selectMainConversions[14].value));
                         break;
                 }
                 break;
@@ -154,10 +161,50 @@ selectMainConversions[0].style.display = 'flex';
 
 // FUNCTIONS
 
-function convertValue(module : number, valueAfter : number) {
+function convertValue(module : number, moduleAfter : string, moduleResult : string) : number {
+    var convertedValue = 0;
     switch(module) {
-        case 1:
-            
+        case 1: // MASSA
+            switch(moduleAfter) {
+                case "kg":
+                    switch(moduleResult) {
+                        case "g":
+                        convertedValue = 168;
+                        break;
+                        case "mg":
+                            break;
+                        case "t":
+                            break;
+                        case "lb":
+                            break;
+                        case "oz":
+                            break;
+                    }
+                    break;
+            }
+            break;
+        case 2: // COMPRIMENTO
+            break;
+        case 3: // TEMPERATURA  
+            break;
+        case 4: // VELOCIDADE
+            break;
+        case 5: // ÁREA
+            break;
+        case 6: // VOLUME
+            break;
+        case 7: // TEMPO
             break;
     }
+
+    return convertedValue;
 }
+
+/*
+            <option value="kg">Quilograma (kg)</option>
+            <option value="g">Grama (g)</option>
+            <option value="mg">Miligrama (mg)</option>
+            <option value="t">Tonelada (t)</option>
+            <option value="lb">Libra (lb)</option>
+            <option value="oz">Onça (oz)</option>
+*/
